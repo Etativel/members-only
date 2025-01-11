14952127d2d27e7caccef9529181d61d994ad77b
Etativel <farhanmaulana4611@gmail.com>
2025-01-11 22:46:06 +0800

    feat: add chat room for gues

--- routes/chatRoom.js
+++ routes/chatRoom.js
@@ -3,23 +3,7 @@ const router = express();
 const messageDb = require("../database/messagesQuery");
 router.use(express.urlencoded({ extended: true }));
 
-// function roleBasedRedirect(req, res, next) {
-//   if (!req.user) {
-//     console.log(req.user);
-//     return res.redirect("/");
-//   }
-//   if (req.user.is_admin) {
-//     res.locals.view = "adminChatRoom";
-//   } else if (req.user.is_member) {
-//     res.locals.view = "memberChatRoom";
-//   } else {
-//     res.locals.view = "guestChatRoom";
-//   }
-//   next();
-// }
-
 function roleBasedRedirect(req, res, next) {
-  // If there's no user, set them as a guest
   if (!req.user) {
     console.log("User is a guest.");
     req.user = {
@@ -27,10 +11,9 @@ function roleBasedRedirect(req, res, next) {
       is_guest: true,
       is_admin: false,
       is_member: false,
-    }; // Set guest properties
+    };
   }
 
-  // Determine the chat room based on user role
   if (req.user.is_admin) {
     res.locals.view = "adminChatRoom";
   } else if (req.user.is_member) {
@@ -38,17 +21,12 @@ function roleBasedRedirect(req, res, next) {
   } else if (req.user.is_guest) {
     res.locals.view = "guestChatRoom";
   } else {
-    res.locals.view = "guestChatRoom"; // Default view
+    res.locals.view = "guestChatRoom";
   }
   next();
 }
 
 router.get("/chat-room", roleBasedRedirect, async (req, res) => {
-  // console.log(req.user);
-  // if (!req.user) {
-  //   console.log(req.user);
-  //   return res.redirect("/");
-  // }
   try {
     console.log(req.user);
     const messages = await messageDb.getAllMessage();
