624bada6b25bc8bfe9460c1909a4470fe6d4baeb
Etativel <farhanmaulana4611@gmail.com>
2025-01-11 22:44:56 +0800

    feat: add chat room for gues

--- routes/chatRoom.js
+++ routes/chatRoom.js
@@ -3,17 +3,42 @@ const router = express();
 const messageDb = require("../database/messagesQuery");
 router.use(express.urlencoded({ extended: true }));
 
+// function roleBasedRedirect(req, res, next) {
+//   if (!req.user) {
+//     console.log(req.user);
+//     return res.redirect("/");
+//   }
+//   if (req.user.is_admin) {
+//     res.locals.view = "adminChatRoom";
+//   } else if (req.user.is_member) {
+//     res.locals.view = "memberChatRoom";
+//   } else {
+//     res.locals.view = "guestChatRoom";
+//   }
+//   next();
+// }
+
 function roleBasedRedirect(req, res, next) {
+  // If there's no user, set them as a guest
   if (!req.user) {
-    console.log(req.user);
-    return res.redirect("/");
+    console.log("User is a guest.");
+    req.user = {
+      username: "Guest",
+      is_guest: true,
+      is_admin: false,
+      is_member: false,
+    }; // Set guest properties
   }
+
+  // Determine the chat room based on user role
   if (req.user.is_admin) {
     res.locals.view = "adminChatRoom";
   } else if (req.user.is_member) {
     res.locals.view = "memberChatRoom";
-  } else {
+  } else if (req.user.is_guest) {
     res.locals.view = "guestChatRoom";
+  } else {
+    res.locals.view = "guestChatRoom"; // Default view
   }
   next();
 }
