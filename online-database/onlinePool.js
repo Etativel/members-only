14f70490a9c8b4afedf7fa58effe96819463103b
Etativel <farhanmaulana4611@gmail.com>
2025-01-16 16:13:26 +0800

    feat: update database

---
+++ online-database/onlinePool.js
@@ -0,0 +1,13 @@
+require("dotenv").config({ path: "./.env" });
+
+const { Pool } = require("pg");
+
+module.exports = new Pool({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
